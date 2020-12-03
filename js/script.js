dd84d69df13301f2144deb85fe9a10b85cf9d428
DavideGit991 <pasto.davide1991@gmail.com>
2020-12-03 16:49:38 +0100

    inserite img dinamicamente in base al DB

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
-// Milestone 1:
-// Creare un layout base con una searchbar (una input e un button) in cui possiamo scrivere completamente o parzialmente il nome di un film. Possiamo, cliccando il  bottone, cercare sull’API tutti i film che contengono ciò che ha scritto l’utente.
-// Vogliamo dopo la risposta dell’API visualizzare a schermo i seguenti valori per ogni film trovato:
-// Titolo
-// Titolo Originale
-// Lingua
-// Voto
+
+
 const api_key= 'da8ab8e676fdc112f397d51f49a3af17'
 const apiOverwiev = 'https://developers.themoviedb.org/3/getting-started/introduction'
 // const apiConfig = 'https://api.themoviedb.org/3/configuration?api_key=da8ab8e676fdc112f397d51f49a3af17'
